fb0e200c05bec610ffc09512e2269ee4a4bd2464
Selim <selimchemutai19@gmail.com>
2021-08-19 13:24:08 +0300

    recycler view adapter

---
+++ app/src/main/java/com/pamela/gads/NoteRecyclerAdapter.kt
@@ -0,0 +1,11 @@
+package com.pamela.gads
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class NoteRecyclerAdapter {
+    class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+
+
+    }
+}
