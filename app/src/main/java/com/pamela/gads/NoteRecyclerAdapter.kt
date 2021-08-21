f39ae06cc4c9f23ba0dc97fcfea37f07dbe35901
Selim <selimchemutai19@gmail.com>
2021-08-21 16:22:03 +0300

    OnCreateViewHolder

--- app/src/main/java/com/pamela/gads/NoteRecyclerAdapter.kt
+++ app/src/main/java/com/pamela/gads/NoteRecyclerAdapter.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.view.menu.MenuView
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.*
 
@@ -11,69 +13,26 @@ class NoteRecyclerAdapter( private val context: Context) : RecyclerView.Adapter<
 
     private val layoutInflater= LayoutInflater.from(context)
 
-    class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
-
-
-    }
-
-    /**
-     * Called when RecyclerView needs a new [ViewHolder] of the given type to represent
-     * an item.
-     *
-     *
-     * This new ViewHolder should be constructed with a new View that can represent the items
-     * of the given type. You can either create a new View manually or inflate it from an XML
-     * layout file.
-     *
-     *
-     * The new ViewHolder will be used to display items of the adapter using
-     * [.onBindViewHolder]. Since it will be re-used to display
-     * different items in the data set, it is a good idea to cache references to sub views of
-     * the View to avoid unnecessary [View.findViewById] calls.
-     *
-     * @param parent The ViewGroup into which the new View will be added after it is bound to
-     * an adapter position.
-     * @param viewType The view type of the new View.
-     *
-     * @return A new ViewHolder that holds a View of the given view type.
-     * @see .getItemViewType
-     * @see .onBindViewHolder
-     */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("Not yet implemented")
-    }
+        //This method is used to create the view that we need
 
-    /**
-     * Called by RecyclerView to display the data at the specified position. This method should
-     * update the contents of the [ViewHolder.itemView] to reflect the item at the given
-     * position.
-     *
-     *
-     * Note that unlike [android.widget.ListView], RecyclerView will not call this method
-     * again if the position of the item changes in the data set unless the item itself is
-     * invalidated or the new position cannot be determined. For this reason, you should only
-     * use the `position` parameter while acquiring the related data item inside
-     * this method and should not keep a copy of it. If you need the position of an item later
-     * on (e.g. in a click listener), use [ViewHolder.getAdapterPosition] which will
-     * have the updated adapter position.
-     *
-     * Override [.onBindViewHolder] instead if Adapter can
-     * handle efficient partial bind.
-     *
-     * @param holder The ViewHolder which should be updated to represent the contents of the
-     * item at the given position in the data set.
-     * @param position The position of the item within the adapter's data set.
-     */
+        var itemView = layoutInflater.inflate(R.layout.item_note_list, parent, false)
+        // each time the onCreate View Holder is called, well inflate the layout recourse to create another view instance
+        // that we can use within the recycler view
+        return ViewHolder(itemView)
+        // we then construct our view holder, passing in that reference to the view going inside the viewholder class
+    }
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        //it is responsible for displaying the data
     }
-
-    /**
-     * Returns the total number of items in the data set held by the adapter.
-     *
-     * @return The total number of items in this adapter.
-     */
     override fun getItemCount(): Int {
         TODO("Not yet implemented")
+    }
+    class ViewHolder (itemView: View): RecyclerView.ViewHolder(itemView) {
+        // Here we get reference to the text we are to use in that particular view holder
+        //the item to be displayed in the views include textcourse and the text title; hence;
+        var textCourse =itemView.findViewById<TextView>(R.id.textCourse)
+        var textTitle=itemView.findViewById<TextView>(R.id.textTitle)
+
     }
 }
